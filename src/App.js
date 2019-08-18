1ae201a03f75dbc58c96351e8551ab1202142301
MarinesLemus007 <marineslemus@gmail.com>
2019-08-18 16:14:59 -0400

    cambios en el css y construyendo el modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Modal from './Modal'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 const ClientKey = '7df746a39c7d1e29b2c68278355a50da016e7e94816ebfd676628c4456dbc917';
@@ -7,118 +8,140 @@ const pathBase = 'https://api.unsplash.com/search/photos';
 
 class Key extends React.Component{
 
-   constructor(props){
-     super(props)
-       this.field ='';
-       this.valueField = this.valueField.bind(this);
-       this.search = this.search.bind(this);
+  constructor(props){
+    super(props)
+      this.field ='';
+      this.valueField = this.valueField.bind(this);
+      this.search = this.search.bind(this);
+      this.openModal = this.openModal.bind(this);
+      this.closeModal = this.closeModal.bind(this);
+      this.state= {
+        images:[],
+        isModalOpen: false
+      }
+  }
 
-       this.state= {
-         images:[]
-       }
-   }
-
-   componentDidMount() {
-
+  componentDidMount() {
     fetch(`https://api.unsplash.com/photos?per_page=1000&client_id=${ClientKey}`)
-
     .then(res=>{
       return res.json()
-  }).then(jsonRes=>{
+    }).then(jsonRes=>{
       console.log(jsonRes);
       this.setState({
         images: jsonRes
       })
-  })
-}
+    })
+  }
 
-   search(){
-       fetch(`${pathBase}?per_page=1000&query=${this.field}&client_id=${ClientKey}`)
-           .then(res=>{
-               return res.json()
-           }).then(jsonRes=>{
-               console.log(jsonRes);
-               this.setState({
-                 images: jsonRes.results
-               })
-           })
-   }
+  search(){
+    fetch(`${pathBase}?per_page=1000&query=${this.field}&client_id=${ClientKey}`)
+    .then(res=>{
+      return res.json()
+    }).then(jsonRes=>{
+      console.log(jsonRes);
+      this.setState({
+        images: jsonRes.results
+      })
+    })
+  }
 
-   valueField(ev){
-       this.field = ev.target.value;
-   }
+  valueField(ev){
+    this.field = ev.target.value;
+  }
 
-   images(){
-return this.state.images.map(i=>{
-  return <img src={i.urls.thumb} alt="No hay imagenes" key={i.id} />
-   
-})
-   }
+  images(){
+    return this.state.images.map(i=>{
+      return (
+        <>
+          <button className="buttom-img" onClick={this.openModal}><img src={i.urls.thumb} alt="No hay imagenes" key={i.id} /></button>
+          {this.state.isModalOpen && <Modal onClose={this.closeModal}/>}
+       </>
+      )
+    })
+  }
+
+  openModal() {
+    this.setState({ isModalOpen: true})
+  }
+  
+  closeModal () {
+    this.setState({ isModalOpen: false })
+  }
+  
 
    render(){
-       return (
-           <>
-            <header>
-               <nav className="nav-container">
-                <ul className="list-nav">
-                <li>
-                  <div className="logo-container">
-                    <i className="fab fa-pinterest"></i>
-                  </div>
-                </li>
-                
-                  <li>
-                  <div className="search-container">  
-                    <button onClick={this.search}><i className="fas fa-search"></i></button>
-                    <input type='text' onChange={this.valueField}/>
-                    <button><i className="fas fa-times"></i></button>
-                    <div className="all-pines">Todos los pines</div>
-                    <button><i className="fas fa-chevron-down"></i></button>
-                    </div>
-                  </li>
+      return (
+        <>
+        <header>
+          <nav className="nav-container">
+            <ul className="list-nav">
               
-                <li>
-                  <div className="friends-container">
-                    <i className="fas fa-user-friends"></i>
+              <li>
+                <div className="logo-container">
+                  <i className="fab fa-pinterest"></i>
+                </div>
+              </li>
+                
+              <li>
+                <div className="search-container">  
+                  <button onClick={this.search}><i className="fas fa-search"></i></button>
+                  <input type='text' placeholder="Buscar" onChange={this.valueField}/>
+                  <button><i className="fas fa-times"></i></button>
+                  <div className="all-pines">Todos los pines
+                    <button><i className="fas fa-chevron-down"></i></button>
                   </div>
-                </li>
-                <li>
-                  <div className="user-container">
-                  <i className="fas fa-circle"></i>
-                  </div>
-                </li>
-                <li>
-                  <div className="divisor-container">
-                    |
-                  </div>
-                </li>
-                <li>
-                  <div className="coment-container">
-                    <i className="fas fa-comment-dots"></i>
-                  </div>
-                </li>
-                <li>
-                  <div className="bell-container">
-                    <i className="fas fa-bell"></i>
-                  </div>
-                </li>
-                <li>
-                  <div className="ellipsis-container">
-                    <i className="fas fa-ellipsis-h"></i>
-                  </div>
-                </li>
-                </ul>
-              </nav>
-            </header>
-            <section>
-              <div className="center-container"> 
-              <div className="img-container">
-                {this.images()}
+                </div>
+              </li>
+              
+              <li>
+                <div className="friends-container">
+                  <i className="fas fa-user-friends"></i>
+                </div>
+              </li>
+              
+              <li>
+                <div className="user-container">
+                <i className="fas fa-circle"></i>
               </div>
-              </div> 
-            </section> 
-           </>
-       )
-   }
+              </li>
+              
+              <li>
+                <div className="divisor-container">
+                  |
+                </div>
+              </li>
+              
+              <li>
+                <div className="coment-container">
+                  <i className="fas fa-comment-dots"></i>
+                </div>
+              </li>
+                
+              <li>
+                <div className="bell-container">
+                  <i className="fas fa-bell"></i>
+                </div>
+              </li>
+              
+              <li>
+                <div className="ellipsis-container">
+                  <i className="fas fa-ellipsis-h"></i>
+                </div>
+              </li>
+                
+            </ul>
+          </nav>
+        </header>
+        
+        <section>
+          <div className="center-container"> 
+            <div className="img-container">
+              {this.images()}
+            </div>
+          </div> 
+        </section> 
+      </>
+    )
+  }
 }
 export default Key;
--- src/Modal.js
+++ src/Modal.js
@@ -1,15 +1,34 @@
+import React from 'react';
 import ReactDOM from 'react-dom'
 
-class Moda extends React.Component{
+class Modal extends React.Component{
     render(){
-        return (
+        return ReactDOM.createPortal(
             <div className="modal">
                 <section className="modal-body">
                     <h1>Modal de Prueba</h1>
                     <p>Modal para las imágenes</p>
-                    <button onClick={this.props.onClosed}>Cerrar</button>
+                    <button onClick={this.props.closedModal}>Cerrar</button>
                 </section>
-            </div>
+            </div>,
+            document.getElementById('modal')
         )
     }
 }
+export default Modal;
+
+// class Modal extends React.Component {
+//     render () {
+//       const { isOpen, onClose } = this.props;
+   
+//       return (
+//         <div className={isOpen ? 'modal modal--is-open' : 'modal'}>
+//           <button onClick={onClose}>close</button>
+   
+//           <p>Hello! I am a modal.</p>
+//         </div>
+//       );
+//     }
+//   }
+  
+//   ReactDOM.render( <Modal />,document.getElementById('react-container'));
